f0bf0aaf1ca147d80279d059df767e3eed1eb11a
Vipul Gupta <66996302+guptavipul0112@users.noreply.github.com>
2020-07-12 14:54:26 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,14 +1,16 @@
 <?php
 
-require_once"try.html";
+require_once"index.html";
 
-$name=$_POST['name'];
-$RollNo=$_POST['RollNo'];
+$uname=$_POST['uname'];
+$pass=$_POST['pass'];
+$mobile=$_POST['mobile'];
+
 
 $servername = "localhost";
-$username = "id10292872_cypher";
-$password = "VinayGupta@123";
-$database = "id10292872_demo";
+$username = "id14055858_localhost";
+$password = "Vip@8600684381";
+$database = "id14055858_login_form";
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $database);
@@ -18,10 +20,12 @@ if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
-$stmt = $conn->prepare("INSERT INTO demo VALUES (?, ?)");
-$stmt->bind_param("ss", $name,$RollNo);
+$stmt = $conn->prepare("INSERT INTO demo VALUES (?, ?, ?)");
+$stmt->bind_param("sss", $uname,$pass,$mobile);
 $stmt->execute();
-echo "New record created successfully";
+echo '<script>
+    window.location="signin.html";
+    </script>';
 
 
 $conn->close();
